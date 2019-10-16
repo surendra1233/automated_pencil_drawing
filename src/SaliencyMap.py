2924ff09ab78a166836362966b6903379a9b24f2
phanirithvij <phanirithvij2000@gmail.com>
2019-10-16 12:45:34 +0530

    gaussian pyr

--- src/SaliencyMap.py
+++ src/SaliencyMap.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 
+
 class SaliencyMap:
     # initialization
     def __init__(self, width, height):
-        self.width  = width
+        self.width = width
         self.height = height
 
-        # extracting color channels
+    # extracting color channels
     def SMExtractRGBI(self, inputImage):
         # convert scale of array elements
         src = np.float32(inputImage) * 1./255
@@ -17,3 +18,11 @@ class SaliencyMap:
         I = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
         # return
         return R, G, B, I
+
+    def FMCreateGaussianPyr(self, src):
+        dst = list()
+        dst.append(src)
+        for i in range(1,4):
+            nowdst = cv2.pyrDown(dst[i-1])
+            dst.append(nowdst)
+        return dst
