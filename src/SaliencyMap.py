ce08b36ea188af7426a4c907ca5ce9618f2a3956
phanirithvij <phanirithvij2000@gmail.com>
2019-10-16 12:43:53 +0530

    init saliency

---
+++ src/SaliencyMap.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+class SaliencyMap:
+    # initialization
+    def __init__(self, width, height):
+        self.width  = width
+        self.height = height
+
+        # extracting color channels
+    def SMExtractRGBI(self, inputImage):
+        # convert scale of array elements
+        src = np.float32(inputImage) * 1./255
+        # split
+        (B, G, R) = cv2.split(src)
+        # extract an intensity image
+        I = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        # return
+        return R, G, B, I
