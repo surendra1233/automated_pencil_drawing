69765e26d50644cbe9e449f9401990c3fdabcb06
phanirithvij <phanirithvij2000@gmail.com>
2019-10-17 09:07:05 +0530

    color feature maps

--- src/SaliencyMap.py
+++ src/SaliencyMap.py
@@ -1,5 +1,5 @@
-import cv2
 import numpy as np
+import cv2
 
 
 class SaliencyMap:
@@ -19,32 +19,55 @@ class SaliencyMap:
         # return
         return R, G, B, I
 
-    ## constructing a Gaussian pyramid
+    # constructing a Gaussian pyramid
     def FMCreateGaussianPyr(self, src):
         dst = list()
         dst.append(src)
-        for i in range(1,9):
+        for i in range(1, 9):
             nowdst = cv2.pyrDown(dst[i-1])
             dst.append(nowdst)
         return dst
 
+    # taking center-surround differences
     def FMCenterSurroundDiff(self, GaussianMaps):
         dst = list()
-        for s in range(2,5):
+        for s in range(2, 5):
             now_size = GaussianMaps[s].shape
-            now_size = (now_size[1], now_size[0])  ## (width, height)
-            tmp = cv2.resize(GaussianMaps[s+3], now_size, interpolation=cv2.INTER_LINEAR)
+            now_size = (now_size[1], now_size[0])  # (width, height)
+            tmp = cv2.resize(
+                GaussianMaps[s+3], now_size, interpolation=cv2.INTER_LINEAR)
             nowdst = cv2.absdiff(GaussianMaps[s], tmp)
             dst.append(nowdst)
-            tmp = cv2.resize(GaussianMaps[s+4], now_size, interpolation=cv2.INTER_LINEAR)
+            tmp = cv2.resize(
+                GaussianMaps[s+4], now_size, interpolation=cv2.INTER_LINEAR)
             nowdst = cv2.absdiff(GaussianMaps[s], tmp)
             dst.append(nowdst)
         return dst
 
+    ## constructing a Gaussian pyramid + taking center-surround differences
     def FMGaussianPyrCSD(self, src):
         GaussianMaps = self.FMCreateGaussianPyr(src)
         dist = self.FMCenterSurroundDiff(GaussianMaps)
         return dist
 
+    ## intensity feature maps
     def IFMGetFM(self, I):
         return self.FMGaussianPyrCSD(I)
+
+    # Color efatuire maps
+    def CFMGetFM(self, R, G, B):
+        tmp1 = cv2.max(R, G)
+        RGBMax = cv2.max(B, tmp1)
+        RGBMax[RGBMax <= 0] = 0.0001
+        # min(R,G)
+        RGMin = cv2.min(R, G)
+        # RG = (R-G)/max(R,G,B)
+        RG = (R - G) / RGBMax
+        # BY = (B-min(R,G)/max(R,G,B)
+        BY = (B - RGMin) / RGBMax
+        RG[RG < 0] = 0
+        BY[BY < 0] = 0
+        RGFM = self.FMGaussianPyrCSD(RG)
+        BYFM = self.FMGaussianPyrCSD(BY)
+        # return
+        return RGFM, BYFM
