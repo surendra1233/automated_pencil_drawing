7ce7e6fdfd4b872bf82e3d5fabf54938b4735bf9
phanirithvij <phanirithvij2000@gmail.com>
2019-11-06 15:53:12 +0530

    comments

--- src/mcgp.py
+++ src/mcgp.py
@@ -34,9 +34,6 @@ if __name__ == "__main__":
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.hist(img.ravel(), 256, [0, 256])
     plt.show()
-    # show_img(cv2.GaussianBlur(img, (31,31), 0))
-    # plt.show()
-    # exit(1)
     out = []
     N = 3
     get_gp(img, out, N)
